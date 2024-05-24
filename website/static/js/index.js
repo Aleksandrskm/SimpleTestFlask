7ad0fe227188a5ac72241bcef9c2c5b025e9a79e
Aleksandr <136788071+Aleksandrskm@users.noreply.github.com>
2024-05-24 04:37:37 +0300

    Update index.js

--- website/static/js/index.js
+++ website/static/js/index.js
@@ -1,3 +1,4 @@
+'use strict';
 function azimuth_and_elevation_angle() {
     const lat1 = document.getElementById('lat1').value;
     const lon1 = document.getElementById('lon1').value;
@@ -5,7 +6,7 @@ function azimuth_and_elevation_angle() {
     const lat2 = document.getElementById('lat2').value;
     const lon2 = document.getElementById('lon2').value;
     const alt2 = document.getElementById('alt2').value;
-  
+
     fetch('/api/ade?lat1=' + lat1 + '&lon1=' + lon1 + '&alt1=' + alt1 + '&lat2=' + lat2 + '&lon2=' + lon2 + '&alt2=' + alt2)
       .then(response => response.json())
       .then(data => {
@@ -83,3 +84,62 @@ function communication_availability() {
         console.error('Error:', error);
       });
    }
+
+async function postJSON(data) {
+    try {
+      const response = await fetch("http://185.192.247.60:7128/Database/TableInfo", {
+        method: "POST", // or 'PUT'
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      const result = await response.json();
+      console.log("Success:", result);
+      return result;
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+let url = 'http://185.192.247.60:7128/Database/DBTables';
+let response =  fetch(url)
+.then(response =>response.json())
+.then(json=>{
+  json.forEach(element => {
+    const elem =document.createElement('div');
+    elem.innerHTML=`<div class="container__nav__el"> ${element}</div>`;
+    elem.addEventListener('click',(e)=>{
+      let content = e.target.innerHTML;
+      // console.log(content);
+      const data = {name:content};
+      document.querySelector('.container_content').innerHTML='';
+      postJSON(data).then(result=>{
+        if (result.total_rows_count==0) {
+          const name=document.createElement('div');
+            name.innerHTML=`<div>${result.name}</div>`;
+            document.querySelector('.container_content').append(name);
+        }
+        result.rows.forEach(element => {
+          if (result.rows[0]===element) {
+            const name=document.createElement('div');
+            name.innerHTML=`<div>${result.name}</div>`;
+            document.querySelector('.container_content').append(name);
+          }
+          const table=document.createElement('tr');
+
+          table.innerHTML+=`<tr></tr>`;
+          document.querySelector('.container_content').append(table);
+          element.forEach(el=>{
+            table.innerHTML+=`<td>${el}</td>
+            `;
+          });
+          // console.log(element);
+          document.querySelector('.container_content').append(table);
+        });
+        // console.log(result.rows);
+      });
+    });
+    document.querySelector('.container__nav').append(elem);
+  });
+  // console.log(json);
+});
