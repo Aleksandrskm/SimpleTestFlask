9f2834446645e9dcb31ad1e036c3530bd6dd727e
Вячеслав <62754486+vbelyavcev@users.noreply.github.com>
2024-10-15 12:10:09 +0300

    сделал корректные типы данных и создал класс для выгрузки

--- website/static/js/index.js
+++ website/static/js/index.js
@@ -1,6 +1,39 @@
 import { table } from "./table.js";
 'use strict';
-
+class dataTle
+{
+  constructor(ID,Naim,Active,TLE_Classification,TLE_Name,TLE_International_class1,TLE_International_class2,TLE_Epoch_year,TLE_Epoch_Time,TLE_Element_Version
+    ,TLE_Nomer_vitka,TLE_line1,TLE_Control_sum_line1,Data_beg,Data_end,TLE_Perv_Proizv,TLE_Vtor_Proizv,TLE_Koef_torm,TLE_Naklon,TLE_Dolgota_uzla,TLE_Ecscentr,
+    TLE_Pericentr,TLE_Mean_Anomaly,TLE_Mean_Motion,TLE_line2,TLE_Control_sum_line2
+  ){
+      this.ID=ID;
+      this.Naim=Naim;
+      this.Active=Active;
+      this.TLE_Classification=TLE_Classification;
+      this.TLE_Name=TLE_Name;
+      this.TLE_International_class1=TLE_International_class1;
+      this.TLE_International_class2=TLE_International_class2;
+      this.TLE_Epoch_year=TLE_Epoch_year;
+      this.TLE_Epoch_Time=TLE_Epoch_Time;
+      this.TLE_Element_Version=TLE_Element_Version;
+      this.TLE_Nomer_vitka=TLE_Nomer_vitka;
+      this.TLE_line1=TLE_line1;
+      this.TLE_Control_sum_line1=TLE_Control_sum_line1;
+      this.Data_beg=Data_beg;
+      this.Data_end=Data_end;
+      this.TLE_Perv_Proizv=TLE_Perv_Proizv;
+      this.TLE_Vtor_Proizv=TLE_Vtor_Proizv;
+      this.TLE_Koef_torm=TLE_Koef_torm;
+      this.TLE_Naklon=TLE_Naklon;
+      this.TLE_Dolgota_uzla=TLE_Dolgota_uzla;
+      this.TLE_Ecscentr=TLE_Ecscentr;
+      this.TLE_Pericentr=TLE_Pericentr;
+      this.TLE_Mean_Anomaly=TLE_Mean_Anomaly;
+      this.TLE_Mean_Motion=TLE_Mean_Motion;
+      this.TLE_line2=TLE_line2;
+      this.TLE_Control_sum_line2=TLE_Control_sum_line2;
+  }
+}
 function azimuth_and_elevation_angle() {
     const lat1 = document.getElementById('lat1').value;
     const lon1 = document.getElementById('lon1').value;
@@ -40,32 +73,41 @@ function get_coordinates() {
       });
 }
 function readLinesValue(fileReader) {
+  let tle= new dataTle();
   let jsonTLE={};
   const arrTLE=[];
-  console.log(fileReader);
+  const arrClassTlEs=[];
+  // console.log(fileReader);
   const lines = (fileReader).split("\r\n");
-  console.log(lines)
+  // console.log(lines)
   let count_line=0;
   for (const line of lines) {
     if (count_line==3) {
       count_line=0
       arrTLE.push(jsonTLE);
       jsonTLE={};
+      arrClassTlEs.push(tle);
+      tle=new dataTle();
     }
-    processLine(line,count_line,jsonTLE);
+    processLine(line,count_line,jsonTLE,tle);
+    
     count_line++;
   }
+  arrTLE.push(jsonTLE);
+  arrClassTlEs.push(tle);
+  console.log(arrClassTlEs);
   console.log(arrTLE);
 }
-function processLine(line,count_line,jsonTLE) {
+function processLine(line,count_line,jsonTLE,tle) {
   let dataTLE;
-  const arrTLENames=['Номер строки_1','Номер спутника в базе данных NORAD_1',
+  
+  const arrTLENames=['Номер строки_1','Kod_NORAD',
     'TLE_Classification','TLE_International_class1',
     'TLE_International_class2',
     'TLE_Epoch_year','TLE_Epoch_Time',
     'TLE_Perv_Proizv','TLE_Vtor_Proizv'
   ,'TLE_Koef_torm',
-  'Изначально — типы эфемерид',
+  'ephemeris_type',
   'TLE_Element_Version',
   'TLE_Control_sum_line1'];
   const arrTLENames_two=['Номер строки_2','Номер спутника в базе данных NORAD_2',
@@ -82,21 +124,25 @@ function processLine(line,count_line,jsonTLE) {
   let counter=0;
   if (count_line==0) {
     jsonTLE['TLE_Name']=dataTLE;
+    tle.TLE_Name=dataTLE;
   }
   else if (count_line==1) {
+    jsonTLE['TLE_line1']=dataTLE;
+    tle.TLE_line1=dataTLE;
     for (let i = 0; i < dataTLE.length; i++) {
       if (i==0) {
         element+=dataTLE[i];
         const nameField=arrTLENames[counter];
-        jsonTLE[nameField]=element;
+        jsonTLE[nameField]=+element;
         element='';
         counter+=1;
       }
       else if (i>1 && i<7) {
         if (i==6) {
+
           element+=dataTLE[i];
           const nameField=arrTLENames[counter];
-          jsonTLE[nameField]=element;
+          jsonTLE[nameField]=+element;
           element='';
           counter+=1;
         }
@@ -108,6 +154,7 @@ function processLine(line,count_line,jsonTLE) {
         element+=dataTLE[i];
         const nameField=arrTLENames[counter];
         jsonTLE[nameField]=element;
+        tle.TLE_Classification=element;
         element='';
         counter+=1;
       } 
@@ -115,7 +162,8 @@ function processLine(line,count_line,jsonTLE) {
         if (i==10) {
           element+=dataTLE[i];
         const nameField=arrTLENames[counter];
-        jsonTLE[nameField]=element;
+        jsonTLE[nameField]=Number(element);
+        tle.TLE_International_class1=Number(element);
         element='';
         counter+=1;
         }
@@ -135,11 +183,12 @@ function processLine(line,count_line,jsonTLE) {
       //     element+=dataTLE[i];
       //   }
       // }
-      else if (i>10 && i<17) {
-        if (i==16) {
+      else if (i>10 && i<15) {
+        if (i==14) {
           element+=dataTLE[i];
         const nameField=arrTLENames[counter];
         jsonTLE[nameField]=element;
+        tle.TLE_International_class2=element;
         element='';
         counter+=1;
         }
@@ -151,12 +200,13 @@ function processLine(line,count_line,jsonTLE) {
         if (i==19) {
           element+=dataTLE[i];
         const nameField=arrTLENames[counter];
-        jsonTLE[nameField]=element;
-        console.log(element)  
+        jsonTLE[nameField]=Number(element);
+        tle.TLE_Epoch_year=Number(element);
+        // console.log(element)  
         element='';
         counter+=1;
-        console.log(arrTLENames[counter]);
-        console.log(counter);
+        // console.log(arrTLENames[counter]);
+        // console.log(counter);
         }
         else{
           element+=dataTLE[i];
@@ -167,8 +217,8 @@ function processLine(line,count_line,jsonTLE) {
         if (i==31) {
           element+=dataTLE[i];
           const nameField=arrTLENames[counter];
-          jsonTLE[nameField]=element;
-         
+          jsonTLE[nameField]=+(element);
+          tle.TLE_Epoch_Time=+(element);
           element='';
          
           counter+=1;
@@ -183,7 +233,12 @@ function processLine(line,count_line,jsonTLE) {
         if (i==42) {
           element+=dataTLE[i];
         const nameField=arrTLENames[counter];
-        jsonTLE[nameField]=element;
+        let correctElem
+        if (element[0]==' ') {
+          correctElem=element.replace(/\s/,'0');
+        }
+        jsonTLE[nameField]=+correctElem;
+        tle.TLE_Perv_Proizv=+correctElem;
         element='';
         counter+=1;
         }
@@ -191,25 +246,60 @@ function processLine(line,count_line,jsonTLE) {
           element+=dataTLE[i];
         }
       }
-      else if (i>43 && i<52) {
-        if (i==51) {
+      else if (i>43 && i<53) {
+        if (i==52) {
+          let correctElem='';
+          let num='',degree='';
+
+          if (element[0]==' ') {
+            correctElem=element.replace(/\s/,'0');
+            let indexDegree=element.search(/[-+]/);
+            
+            for (let i = 0; i < indexDegree; i++) {
+              num+=correctElem[i];
+            }
+            
+            for (let index = indexDegree; index < correctElem.length; index++) {
+               degree += correctElem[index];
+              
+            }
+          }
           element+=dataTLE[i];
-        const nameField=arrTLENames[counter];
-        jsonTLE[nameField]=element;
-        element='';
-        counter+=1;
+          const nameField=arrTLENames[counter];
+          jsonTLE[nameField]=Math.pow(+num,+degree);
+          tle.TLE_Vtor_Proizv=Math.pow(+num,+degree);
+          element='';
+          counter+=1;
         }
         else{
           element+=dataTLE[i];
         }
       }
-      else if (i>52 && i<61) {
-        if (i==60) {
+      else if (i>52 && i<62) {
+        if (i==61) {
+          let correctElem='';
+          let num='',degree='';
+
+          if (element[0]==' ') {
+            correctElem=element.replace(/\s/,'0.');
+            // console.log(element)
+            let indexDegree=correctElem.search(/[-+]/);
+            
+            for (let i = 0; i < indexDegree; i++) {
+              num+=correctElem[i];
+            }
+            
+            for (let index = indexDegree; index < correctElem.length; index++) {
+               degree += correctElem[index];
+              
+            }
+          }
           element+=dataTLE[i];
-        const nameField=arrTLENames[counter];
-        jsonTLE[nameField]=element;
-        element='';
-        counter+=1;
+          const nameField=arrTLENames[counter];
+          jsonTLE[nameField]=Math.pow(+num,+degree);
+          tle.TLE_Koef_torm=Math.pow(+num,+degree);
+          element='';
+          counter+=1;
         }
         else{
           element+=dataTLE[i];
@@ -219,7 +309,8 @@ function processLine(line,count_line,jsonTLE) {
         
         element+=dataTLE[i];
         const nameField=arrTLENames[counter];
-        jsonTLE[nameField]=element;
+        jsonTLE[nameField]=+element;
+        
         element='';
         counter+=1;
         
@@ -228,7 +319,8 @@ function processLine(line,count_line,jsonTLE) {
         if (i==67) {
           element+=dataTLE[i];
         const nameField=arrTLENames[counter];
-        jsonTLE[nameField]=element;
+        jsonTLE[nameField]=+element;
+        tle.TLE_Element_Version=+element;
         element='';
         counter+=1;
         }
@@ -240,7 +332,8 @@ function processLine(line,count_line,jsonTLE) {
         
         element+=dataTLE[i];
         const nameField=arrTLENames[counter];
-        jsonTLE[nameField]=element;
+        jsonTLE[nameField]=+element;
+        tle.TLE_Control_sum_line1=+element;
         element='';
         counter+=1;
         
@@ -249,11 +342,13 @@ function processLine(line,count_line,jsonTLE) {
     }
   }
   else if (count_line==2) {
+    jsonTLE['TLE_line2']=dataTLE;
+    tle.TLE_line2=dataTLE;
     for (let i = 0; i < dataTLE.length; i++) {
       if (i==0) {
         element+=dataTLE[i];
         const nameField=arrTLENames_two[counter];
-        jsonTLE[nameField]=element;
+        jsonTLE[nameField]=+element;
         element='';
         counter+=1;
       }
@@ -261,7 +356,7 @@ function processLine(line,count_line,jsonTLE) {
         if (i==6) {
           element+=dataTLE[i];
           const nameField=arrTLENames_two[counter];
-          jsonTLE[nameField]=element;
+          jsonTLE[nameField]=+element;
           element='';
           counter+=1;
         }
@@ -274,7 +369,8 @@ function processLine(line,count_line,jsonTLE) {
         if (i==15) {
           element+=dataTLE[i];
         const nameField=arrTLENames_two[counter];
-        jsonTLE[nameField]=element;
+        jsonTLE[nameField]=+element;
+        tle.TLE_Naklon=+element;
         element='';
         counter+=1;
         }
@@ -282,16 +378,17 @@ function processLine(line,count_line,jsonTLE) {
           element+=dataTLE[i];
         }
       }
-      else if (i>16 && i<26) {
-        if (i==25) {
+      else if (i>16 && i<25) {
+        if (i==24) {
           element+=dataTLE[i];
         const nameField=arrTLENames_two[counter];
-        jsonTLE[nameField]=element;
-        console.log(element)  
+        jsonTLE[nameField]=parseFloat(element);
+        tle.TLE_Dolgota_uzla=+element;
+        // console.log(element)  
         element='';
         counter+=1;
-        console.log(arrTLENames_two[counter]);
-        console.log(counter);
+        // console.log(arrTLENames_two[counter]);
+        // console.log(counter);
         }
         else{
           element+=dataTLE[i];
@@ -302,8 +399,8 @@ function processLine(line,count_line,jsonTLE) {
         if (i==32) {
           element+=dataTLE[i];
           const nameField=arrTLENames_two[counter];
-          jsonTLE[nameField]=element;
-         
+          jsonTLE[nameField]=Number('0.'+element);
+          tle.TLE_Ecscentr=Number('0.'+element);
           element='';
          
           counter+=1;
@@ -318,7 +415,8 @@ function processLine(line,count_line,jsonTLE) {
         if (i==41) {
           element+=dataTLE[i];
         const nameField=arrTLENames_two[counter];
-        jsonTLE[nameField]=element;
+        jsonTLE[nameField]=+element;
+        tle.TLE_Pericentr=+element;
         element='';
         counter+=1;
         }
@@ -326,11 +424,12 @@ function processLine(line,count_line,jsonTLE) {
           element+=dataTLE[i];
         }
       }
-      else if (i>42 && i<51) {
+      else if (i>43 && i<51) {
         if (i==50) {
           element+=dataTLE[i];
         const nameField=arrTLENames_two[counter];
-        jsonTLE[nameField]=element;
+        jsonTLE[nameField]=+element;
+        tle.TLE_Mean_Anomaly=+element;
         element='';
         counter+=1;
         }
@@ -342,7 +441,8 @@ function processLine(line,count_line,jsonTLE) {
         if (i==60) {
           element+=dataTLE[i];
         const nameField=arrTLENames_two[counter];
-        jsonTLE[nameField]=element;
+        jsonTLE[nameField]=+element;
+        tle.TLE_Mean_Motion=+element;
         element='';
         counter+=1;
         }
@@ -354,7 +454,8 @@ function processLine(line,count_line,jsonTLE) {
         if (i==67) {
           element+=dataTLE[i];
         const nameField=arrTLENames_two[counter];
-        jsonTLE[nameField]=element;
+        jsonTLE[nameField]=+element;
+        tle.TLE_Nomer_vitka=+element;
         element='';
         counter+=1;
         }
@@ -366,13 +467,15 @@ function processLine(line,count_line,jsonTLE) {
         
         element+=dataTLE[i];
         const nameField=arrTLENames_two[counter];
-        jsonTLE[nameField]=element;
+        jsonTLE[nameField]=+element;
+        tle.TLE_Control_sum_line2=+element;
         element='';
         counter+=1;
         
       }
       
     }
+  
   }
   
  
@@ -454,7 +557,7 @@ document.addEventListener('DOMContentLoaded', function() {
           // создаем объект FileReader для считывания файла
           const reader = new FileReader();
           // console.log(arrTLENames[7]);
-          console.log(reader);
+          // console.log(reader);
           // при успешном чтении файла выводим его содержимое на веб-страницу
           reader.onload = () => {  
                 // выводим содержимое
