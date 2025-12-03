93960f0cf2c53784a9e08db6642e515b663d3b31
Вячеслав <62754486+vbelyavcev@users.noreply.github.com>
2025-12-03 15:54:59 +0300

    сделал новый вид модалки + поменял вид таблиц

--- website/static/js/beams.js
+++ website/static/js/beams.js
@@ -1,21 +1,27 @@
 'use strict';
 import {editRow,deleteRow,insertRow,postJSON,getRowsTable,changeQuery,selectQuery} from './db.js';
 document.addEventListener('DOMContentLoaded',function(){
-    function drawsTrBeam(tr,flagSelected=true,color='black'){
+    function drawsTrBeam(tr,flagSelected=true,color='black',lineWidth = 1){
         if(flagSelected){
             tr.classList.add('selected');
         }
         const distanceBeam=tr.children[2].innerHTML;
-        let centerY=0,centerX=0,radius=(tr.children[3].innerHTML/1000)*0.125;
+        let centerY=0,centerX=0,radius=(tr.children[4].innerHTML/1000)*0.125;
         console.log(tr.children[3].innerHTML)
         console.log(tr.children[4].innerHTML)
         const widthCanvas = document.getElementById('canvas').offsetWidth;
         const heightCanvas = document.getElementById('canvas').offsetHeight;
-        centerY=widthCanvas/2+(distanceBeam*0.125)/1000+radius*Math.sin(tr.children[1].innerHTML* (Math.PI/180));
-        centerX=heightCanvas/2+(distanceBeam*0.125)/1000+radius*Math.cos(tr.children[1].innerHTML* (Math.PI/180));
+        centerY=widthCanvas/3+(distanceBeam*0.125)/1000+radius*Math.sin(tr.children[3].innerHTML* (Math.PI/180));
+        centerX=heightCanvas/3+(distanceBeam*0.125)/1000+radius*Math.cos(tr.children[3].innerHTML* (Math.PI/180));
         console.log('centerX',centerX,'centerX',centerY,'radius',radius)
-        drawCircle(centerX,centerY,radius,color)
+        drawCircle(centerX,centerY,radius,color,lineWidth)
     }
+    function closeTab() {
+        // Создаем пустую страницу и сразу закрываем
+        // window.history.back();
+        window.close();
+    }
+    document.getElementById('exit').addEventListener('click', closeTab);
     function selectFirstRow(){
         const tr=document.querySelector('.KA-Table tbody tr');
         tr.classList.add('selected');
@@ -48,7 +54,7 @@ document.addEventListener('DOMContentLoaded',function(){
         getRowsTable('KA',0,99999).then(KA=>{
           const data = { name: 'KA' };
           const rusName={};
-          document.querySelector('.tab-Ka-name').innerHTML=`Наименование таблицы: ${data.name}`;
+          document.querySelector('.tab-Ka-name').innerHTML=`Список ${data.name}:`;
           postJSON(data).then(tableInfo=>{
             const tr = document.createElement('tr');
             for( let i=0;i<tableInfo.columns_info.length;i++){
@@ -129,7 +135,16 @@ document.addEventListener('DOMContentLoaded',function(){
         }
           else{
               const data = { name: `${selectedValue}`};
-              document.querySelector('.tab-Beams-name').innerHTML=`Наименование таблицы: ${data.name}`;
+              let typeBeams='';
+              if (data.name==='KA_BEAM_PRD')
+              {
+                  typeBeams='по передачи'
+              }
+              else {
+                  typeBeams='по приему'
+              }
+
+              document.querySelector('.tab-Beams-name').innerHTML=`Характеристики лучей ${typeBeams}`;
               console.log(data)
               const rusName={};
               postJSON(data).then(tableInfo=>{
@@ -143,11 +158,17 @@ document.addEventListener('DOMContentLoaded',function(){
                           rusName[tableInfo.columns_info[i].name]=tableInfo.columns_info[i].description;
                           let decr_rus=tableInfo.columns_info[i].description;
                           switch (decr_rus) {
-                              case 'Идентификатор':
-                                  decr_rus='Идентификатор'
+                              case 'Номер луча приема':
+                                  decr_rus='Номер луча'
                                   break;
-                              case 'Азимут луча, градус':
-                                  decr_rus='Азимут от подсп.точки'
+                              case 'Номер луча передачи':
+                                  decr_rus='Номер луча'
+                                  break;
+                              case 'Дальность центра точки луча от подспутниковой точки, метры':
+                                  decr_rus='Дальность центра  луча от подспутниковой точки КА, метры'
+                                  break;
+                              case 'Азимут центр точки луча на поверхности, градус':
+                                  decr_rus='Азимут луча относительно орбиты КА, метры'
                                   break;
 
                               default:
@@ -160,6 +181,14 @@ document.addEventListener('DOMContentLoaded',function(){
 
 
                   }
+                  const trArr=[...tr.children];
+                  console.log(trArr,'children');
+                  const trArrCorrect=[trArr[0],
+                      trArr[4],
+                      trArr[2],
+                      trArr[1],
+                      trArr[3]]
+                  tr.append(...trArrCorrect)
                   document.querySelector('.beams-Table thead').append(tr);
               });
               console.log(selectedValue)
@@ -171,28 +200,38 @@ document.addEventListener('DOMContentLoaded',function(){
              // }
               console.log(idKa)
               selectQuery(`SELECT * FROM ${selectedValue} WHERE ID_KA = ${idKa}`).then((dataBeams)=>{
-                  dataBeams.forEach((beams)=>{
-                      const tr=document.createElement('tr');
-
-                      beams.forEach((dataBeam,index)=>{
-
-                          if(index>1 && index <6 || index === 8){
-                              const td=document.createElement('td');
-                              td.textContent=dataBeam;
-                              tr.append(td);
-                              tr.dataset.id=beams[0];
-                          }
-                          // if (index===1){
-                          //     console.log('dataBeam',beams);
-                          //     tr.dataset.id=beams[0];
-                          // }
-
-                      })
+                  dataBeams.forEach((beams) => {
+                      const tr = document.createElement('tr');
                       tr.classList.add('beams-element');
 
-                      document.querySelector('.beams-Table tbody').append(tr);
+                      const tdArray = []; // Массив для хранения td
 
-                  })
+                      beams.forEach((dataBeam, index) => {
+                          if (index > 1 && index < 6 || index === 8) {
+                              const td = document.createElement('td');
+                              td.textContent = dataBeam;
+                              tdArray.push(td); // Добавляем td в массив
+                          }
+                      });
+
+                      // Переупорядочиваем массив td: 0, 4, 2, 1, 3
+                      const orderedTdArray = [
+                          tdArray[0],
+                          tdArray[4],
+                          tdArray[2],
+                          tdArray[1],
+                          tdArray[3]
+                      ];
+                      orderedTdArray[3].innerHTML=(Number(orderedTdArray[3].innerHTML).toFixed(2));
+                      // Добавляем td в строку в нужном порядке
+                      tr.append(...orderedTdArray);
+
+                      // Устанавливаем dataset.id
+                      tr.dataset.id = beams[0];
+
+                      // Вставляем строку в таблицу
+                      document.querySelector('.beams-Table tbody').append(tr);
+                  });
                   const trsBeams=document.querySelectorAll('.beams-Table tbody tr');
                   trsBeams.forEach((tr)=>{
                       console.log('5',tr.children[4].innerHTML)
@@ -207,7 +246,7 @@ document.addEventListener('DOMContentLoaded',function(){
                           trs.forEach((tr,index)=>{
                               if (tr===e.target.parentElement) {
                                    clearCanvas();
-                                  drawsTrBeam(tr,true,'blue')
+                                  drawsTrBeam(tr,true,'blue',5)
                               }
                               else{
                                   tr.classList.remove('selected');
@@ -264,12 +303,13 @@ document.addEventListener('DOMContentLoaded',function(){
         // Формируем итоговый запрос в одну строку
         return `UPDATE ${tableName} SET ${caseStatements} WHERE ID IN (${arrIdBeams.join(', ')}) AND ID_KA = ${idKa};`;
     }
-    function drawCircle(centerY,centerX,radius,color) {
+    function drawCircle(centerY,centerX,radius,color,lineWidth = 1 ) {
         const canvas = document.getElementById("canvas");
         const ctx = canvas.getContext('2d');
         ctx.beginPath();
         ctx.arc(centerY, centerX, radius, 0, 2 * Math.PI);
         ctx.strokeStyle = color;
+        ctx.lineWidth = lineWidth;
         ctx.stroke();
       }
    
@@ -304,6 +344,11 @@ document.addEventListener('DOMContentLoaded',function(){
         }
 
     })
+    function formatLastThree(str) {
+        str = String(str);
+        if (str.length <= 3) return str; // Не меняем короткие строки
+        return str.slice(0, -3) + ',' + str.slice(-3);
+    }
     document.getElementById('edit-beams').addEventListener('click',()=>{
 
         document.querySelector('.modal-columns-edit').innerHTML=``;
@@ -312,13 +357,52 @@ document.addEventListener('DOMContentLoaded',function(){
         console.log(ths)
         if (beamSelected) {
             document.querySelector('.modal-beams-edit').classList.toggle('close-modal');
+            const dataColum=document.createElement('div');
+            dataColum.classList.add('data-column');
+            let typeBeam='';
+            if(document.querySelector('input[name="type_beams"]:checked').value === 'KA_BEAM_PRD')
+            {
+                typeBeam='Передача'
+            }
+            else {
+                typeBeam='Прием'
+            }
+            dataColum.innerHTML+=`<div class="name-column">Тип луча:</div>`;
+            dataColum.innerHTML+=`<div>${typeBeam}</div>`;
+            document.querySelector('.modal-columns-edit').append(dataColum)
             Array.from(beamSelected.children).forEach((child,index)=>{
 
                     const dataColum=document.createElement('div');
                     dataColum.classList.add('data-column');
-                    dataColum.innerHTML+=`<div class="name-column">${ths[index].innerHTML}</div>`;
-                    dataColum.innerHTML+=`<input value='${child.innerHTML}'></input>`;
+
+                    // dataColum.innerHTML+=`<div class="name-column">${ths[index].innerHTML}</div>`;
+                    if (index===0){
+                        dataColum.innerHTML+=`<div class="name-column">${ths[index].innerHTML}</div>`;
+                        dataColum.innerHTML+=`<input disabled value='${child.innerHTML}'></input>`;
+
+                    }
+                    else if (index===beamSelected.children.length-1 || index===2){
+                        if(index===beamSelected.children.length-1){
+                            dataColum.innerHTML+=`<div class="name-column">${ths[index].innerHTML.replace(/м/g, 'км')}</div>`;
+                        }
+                        else {
+                            dataColum.innerHTML+=`<div class="name-column">${ths[index].innerHTML.replace(/метры/g, 'км')}</div>`;
+                        }
+
+                        dataColum.innerHTML+=`<input value='${formatLastThree(child.innerHTML)}'></input>`;
+
+                    }
+                    else {
+                        dataColum.innerHTML+=`<div class="name-column">${ths[index].innerHTML}</div>`;
+                        dataColum.innerHTML+=`<input value='${child.innerHTML}'></input>`;
+
+                    }
+
+
                     console.log(child.innerHTML)
+                    if (index===1){
+                        dataColum.innerHTML+=` <button disabled id="calcCenterBeam">Расчет дальности центра луча</button>`;
+                    }
                     document.querySelector('.modal-columns-edit').append(dataColum)
 
 
@@ -404,12 +488,12 @@ document.addEventListener('DOMContentLoaded',function(){
         const dataColumns=document.querySelectorAll('.data-column input');
         const beamSelected=document.querySelector('tr.beams-element.selected');
         dataColumns.forEach((data,i)=>{
-                beamSelected.children[i].innerHTML=`<td>${data.value}</td>`
+                beamSelected.children[i].innerHTML=`<td>${data.value.replace(/,/g, '')}</td>`
                 console.log(data.value)
         })
 
         clearCanvas();
-        drawsTrBeam(beamSelected,false,'blue')
+        drawsTrBeam(beamSelected,false,'blue',5)
         document.querySelector('.modal-beams-edit').classList.toggle('close-modal');
 
     })
